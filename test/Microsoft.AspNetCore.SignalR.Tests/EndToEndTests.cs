17da49b8bf8c09e88ba2b59070d1eee7e325609b
Mikael Mengistu <mimengis@microsoft.com>
2017-02-09 11:11:11 -0800

    PR feedback

--- test/Microsoft.AspNetCore.SignalR.Tests/EndToEndTests.cs
+++ test/Microsoft.AspNetCore.SignalR.Tests/EndToEndTests.cs
@@ -91,9 +91,8 @@ namespace Microsoft.AspNetCore.SignalR.Tests
 
                 var received = await ReceiveMessage(connection);
                 
-                Assert.Equal(message, received.Substring(0,message.Length));
+                Assert.Equal(message, received.Substring(0, message.Length));
             }
         }
-
     }
 }
